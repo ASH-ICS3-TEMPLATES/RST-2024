2fc5777e1daf829bf6d5ca165e6bef3e2619215a
Jamieson <158116310+ash-comsci@users.noreply.github.com>
2025-01-08 08:50:20 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+/**
+ * ICS3 - Mr. Brash 🐿️
+ * RICH SUMMATIVE TASK 2024-25 S1
+ *
+ * Description:
+ * 
+ * Author:
+ **/ 
+
+
+'use strict';
+
+// A very accurate rounding function
+function round(value, decimals) {
+    let multiplier = 10**decimals;
+    return Math.round((value + Number.EPSILON) * multiplier) / multiplier;
+}
+
+// Get a random number from min to max
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* 
+    Sleep the code for a certain number of ms
+    NOTE: Any function that will use this must be declared with async and
+    this sleep function called with "await sleep(x)" where x is a number of ms
+*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
